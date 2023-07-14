825568ffc0aedc492dbe90bbf0c07b2226e132c9
flavianahmias <flaviaanahmias@gmail.com>
2023-07-14 19:37:08 -0400

    feat: configure routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.scss";
 import Home from "./pages/home";
+import Header from "./components/header";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+export const Content = () => {
+  return (
+    <div className={styles.content}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 function App() {
-  return <Home />;
+  return (
+    <div className={styles.main}>
+      <Header />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,17 +2,8 @@ import React from "react";
 import Header from "../../components/header";
 import styles from "./styles.module.scss";
 
-export const Content = () => {
-  return <div className={styles.content}>content</div>;
-};
-
 function Home() {
-  return (
-    <div className={styles.main}>
-      <Header />
-      <Content />
-    </div>
-  );
+  return <div className={styles.homepage}>teste</div>;
 }
 
 export default Home;

11fb86accb589cf014ac1d048d06945bb6930369
flavianahmias <flaviaanahmias@gmail.com>
2023-07-14 19:56:20 -0400

    feat: create initial section homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,37 @@
 import React from "react";
 import Header from "../../components/header";
 import styles from "./styles.module.scss";
+import { ReactComponent as CoffeeImage } from "../../assets/images/Imagem.svg";
 
 function Home() {
-  return <div className={styles.homepage}>teste</div>;
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.initial}>
+        <div className={styles.text}>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+
+          <span>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+          </span>
+
+          <div className={styles.details}>
+            <p>Compra simples e segura</p>
+            <p>Compra simples e segura</p>
+            <p>Compra simples e segura</p>
+            <p>Compra simples e segura</p>
+          </div>
+        </div>
+
+        <CoffeeImage />
+      </section>
+
+      <section className={styles.catalog}>
+        <h1> Nossos cafés</h1>
+
+        <div>cafes</div>
+      </section>
+    </div>
+  );
 }
 
 export default Home;

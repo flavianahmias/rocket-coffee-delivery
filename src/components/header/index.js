c10cd67784169e8a2da88fc545428a9dd7295237
flavianahmias <flaviaanahmias@gmail.com>
2023-07-14 19:30:11 -0400

    feat: add header items

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { ReactComponent as Logo } from "../../assets/icons/Logo.svg";
+import { MdLocationPin } from "react-icons/md";
+import { BsFillCartFill } from "react-icons/bs";
 
 function Header() {
-  return <></>;
+  return (
+    <header className={styles.content}>
+      <Logo className={styles.logo} />
+
+      <div className={styles.options}>
+        <div className={styles.location}>
+          <MdLocationPin /> <span>Manaus, AM</span>
+        </div>
+
+        <button className={styles.car}>
+          <BsFillCartFill />
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Header from "../../components/header";
 import styles from "./styles.module.scss";
 
+export const Content = () => {
+  return <div className={styles.content}>content</div>;
+};
+
 function Home() {
-  return <></>;
+  return (
+    <div className={styles.main}>
+      <Header />
+      <Content />
+    </div>
+  );
 }
 
 export default Home;

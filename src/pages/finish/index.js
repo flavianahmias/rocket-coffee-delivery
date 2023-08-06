92ba97da85149cbe18f666289cb1b8d937fa6be1
flavianahmias <flaviaanahmias@gmail.com>
2023-08-06 14:39:01 -0400

    feat: create finish page

--- src/pages/finish/index.js
+++ src/pages/finish/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
+import image from "../../assets/images/Illustration.png";
+
 function FinishPage() {
-  return <></>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <h3>Uhuu! Pedido confirmado!</h3>
+        <span>Agora é só aguardar que logo o café chegará até você</span>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.infos}>
+          <div className={styles.block}>
+            <span>
+              Entrega em <strong>Rua dos passes, 106</strong>
+            </span>
+            <span>Parque 10 - Manaus, AM</span>
+          </div>
+          <div className={styles.block}>
+            <span>Previsão de entrega</span>
+            <span>
+              <strong>20 min - 30 min</strong>
+            </span>
+          </div>
+          <div className={styles.block}>
+            <span>Pagamento na entrega</span>
+            <span>
+              <strong>Cartão de crédito</strong>
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.image}>
+          <img src={image} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default FinishPage;
